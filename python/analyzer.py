150d9e81585fd896dcbbdeb543291f425af6b780
Sam <sambrands2001@gmail.com>
2021-01-13 11:49:41 +0100

    Added detailed comments

--- python/analyzer.py
+++ python/analyzer.py
@@ -11,26 +11,26 @@ matches = pd.read_excel("../files/Hockey_Tweede_klasse_tussenstand.xlsx")
 matches["datum"] = pd.to_datetime(matches["datum"], format="%d/%m/%Y")
 matches = matches.sort_values("datum")
 
-#Informatievraag 1
+#Informatievraag 1: Totaal aantal overtredingen
 sumFile = open("../files/sum.txt", "w", encoding="UTF-8")
 totalViolations = matches["overtredingen"].sum()
 sumFile.write(str(totalViolations))
 sumFile.close()
 
-#Informatievraag 2
+#Informatievraag 2: Gemiddelde aantal overtredingen
 averageFile = open("../files/average.txt", "w", encoding="UTF-8")
 meanViolations = matches["overtredingen"].mean()
 averageFile.write(str(round(meanViolations)))
 averageFile.close()
 
-#Informatievraag 3
+#Informatievraag 3: Zwartboek (wedstrijden met meeste overtredingen)
 zwartBoekFile = open("../files/zwartboek.txt", "w", encoding="UTF-8")
 zwartBoekSorted = matches.sort_values("overtredingen", ascending=False)
 zwartBoek = zwartBoekSorted.head(5)
 zwartBoekFile.write(bamboo.prettify(zwartBoek, type="zwartboek"))
 zwartBoekFile.close()
 
-#Informatievraag 4
+#Informatievraag 4: Eregalerij (wedstrijden met minder dan 2 overtredingen in de afgelopen 14 dagen)
 eregalerijFile = open("../files/eregalerij.txt", "w", encoding="UTF-8")
 checkDays = 14
 date_check = datetime.now() - timedelta(days=checkDays)
@@ -45,5 +45,7 @@ eregalerij = matches_filtered[filter2]
 
 eregalerijFile.write(bamboo.prettify(eregalerij, type="eregalerij"))
 eregalerijFile.close()
+
+os.system("cls")
 
 print("Done!")
